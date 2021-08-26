4f8d01b87a8512aedd1eab6468f7340abd2aacd6
Juna Salviati <antigones@users.noreply.github.com>
2021-08-26 17:26:59 +0200

    toConsole changed

--- binary.py
+++ binary.py
@@ -2,27 +2,31 @@
 
 import numpy as np
 
+
 def toConsole(grid):
     output_grid = np.empty([size*3, size*3],dtype=str)
     output_grid[:] = '#'
-
-    i = 1
-    j = 1
-    while i < (size*3):
-        while j < (size*3):
-            toss = grid[i//3,j//3]
-            output_grid[i,j] = ' '
-            if toss == 0 and j+2 < size*3:
-                output_grid[i,j+1] = ' '
-                output_grid[i,j+2] = ' '
-            if toss == 1 and i-2 >=0:
-                output_grid[i-1,j] = ' '
-                output_grid[i-2,j] = ' '
+    result = [(i, j) for i in range(size) for j in range(size)]
+    print(result)
+    i = 0
+    j = 0
+    while i < size:
+        w = i*3 + 1
+        while j < size:
+            k = j*3 + 1
+            toss = grid[i,j]
+            output_grid[w,k] = ' '
+            if toss == 0 and k+2 < size*3:
+                output_grid[w,k+1] = ' '
+                output_grid[w,k+2] = ' '
+            if toss == 1 and w-2 >=0:
+                output_grid[w-1,k] = ' '
+                output_grid[w-2,k] = ' '
             
-            j = j + 3
+            j = j + 1
             
-        i = i + 3
-        j = 1
+        i = i + 1
+        j = 0
         
     for elm in output_grid:
         print(" ".join(elm))
@@ -33,7 +37,7 @@ p=0.5
 size=5
 
 # 1 (head) N, 0 (tail) E
-#np.random.seed(42)
+np.random.seed(42)
 grid = np.random.binomial(n,p, size=(size,size))
 
 # fix first row and last column to avoid digging outside the maze external borders
